7072c5d59872d89cc4dc02b5eb2d7e3be1a6f850
ProtossManse <ingan2473@gmail.com>
2021-06-17 14:58:09 +0900

    Update Auto_Submit_dev1.py

--- Auto_Submit_dev1.py
+++ Auto_Submit_dev1.py
@@ -1,8 +1,12 @@
+from PyQt5 import QtWidgets
 import pyautogui as pag
 import sys
 import os
 import keyboard
 import webbrowser
+import srcomapi, srcomapi.datatypes as dt
+from nbt.nbt import NBTFile
+import getpass
 
 import PyQt5
 from PyQt5.QtGui import *
@@ -11,7 +15,12 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5 import QtTest
 
-f3p = 0
+username = getpass.getuser()
+userpath = os.path.join("C:\\Users",username,"AppData\\Roaming")
+path = userpath
+
+
+
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
@@ -30,13 +39,35 @@ class MainDialog(QMainWindow):
         self.resetButton.clicked.connect(self.reset)
         self.startButton.setShortcut("Alt+3")
         self.startButton.clicked.connect(self.macro1)
+        self.pathButton.clicked.connect(self.browse)
         self.hook = keyboard.on_press(self.keyboardEventReceived)
+        self.pathLine.setText(userpath +"/.minecraft")
+        dat = NBTFile(os.path.join(path, ".minecraft\\saves\\새로운 세계 (26)\\level.dat"))
+
+        mc_version = str(dat["Data"]["Version"]["Name"])
+        mc_diffi = str(dat["Data"]["Difficulty"])
+        mc_hardcore = str(dat["Data"]["hardcore"])
+        mc_igt = str(dat["Data"]["DayTime"])
+        mc_seed = str(dat["Data"]["WorldGenSettings"]["seed"])
+
+        if mc_hardcore == "1":
+            mc_diffi = "Hardcore"
+        elif mc_hardcore == "0":
+            if mc_diffi == "1":
+                mc_diffi = "Easy"
+            elif mc_diffi == "2":
+                mc_diffi = "Normal"
+            elif mc_diffi == "3":
+                mc_diffi = "Hard"
+        
+
+        print(f"{str(mc_version)}\n{str(mc_diffi)}\n{mc_igt} ticks\n{int(mc_igt)/20} secs\nSeed: {mc_seed}")
     def seedClicked(self):
         seed, ok1 = QInputDialog.getText(self, "Change Seed", "<font face=\"Malgun Gothic\">Seed:</font>")
 
         if ok1 == True:
-            print(seed)
-            self.sText.setText("Seed: " + str(seed))
+            if seed != "":
+                self.sText.setText("Seed: " + str(seed))
 
     def f3Clicked(self):
         if self.f3Box.isChecked() == False:
@@ -50,10 +81,69 @@ class MainDialog(QMainWindow):
         if self.f3Box.isChecked() == True:
             self.f3Box.toggle()
 
+    def browse(self):
+        global path
+        pathops = QFileDialog.Options()
+        pathops |= QFileDialog.ShowDirsOnly
+        path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Browse...', userpath)
+        if path != "":
+            self.pathLine.setText(str(path))
+#--------------------------
+
+    # def get_last_played_level():
+    #     mc_dir = path
+    #     mc_saves = os.path.join(mc_dir, "saves")
+
+    #     worlds_recently_modified = sorted([os.path.join(mc_saves, s) for s in os.listdir(mc_saves)], key=os.path.getmtime, reverse=True)
+    #     for w in worlds_recently_modified.copy()[:3]:
+    #         try:
+    #             world = w
+    #             level = NBTFile(os.path.join(world, "level.dat"))
+    #             if not int(str(level["Data"]["Time"])):
+    #                 continue
+    #             else:
+    #                 break
+    #         except:
+    #             continue
+    #     except: #* If it's pre 1.7.2
+    #         stats = None
+
+    #     try:
+    #         seen_credits = bool(int(str(level["Data"]["Player"]["seenCredits"])))
+    #     except: #* If it's pre 1.12 OR a server
+    #         seen_credits = None
+
+    #     try:
+    #         data = {
+    #             "name": str(level["Data"]["LevelName"]),
+    #             "version": str(level["Data"]["Version"]["Name"]),
+    #             "igt": stats["stat.playOneMinute"] if int(str(level["Data"]["DataVersion"])) < 1451 else stats["stats"]["minecraft:custom"]["minecraft:play_one_minute"],
+    #             "seen_credits": seen_credits,
+    #             "pre17": False
+    #         }
+    #     except: #* If it's pre 1.9
+    #         try:
+    #             data = {
+    #                 "name": str(level["Data"]["LevelName"]),
+    #                 "version": "Pre 1.9",
+    #                 "igt": stats["stat.playOneMinute"],
+    #                 "seen_credits": seen_credits,
+    #                 "pre17": False
+    #             }
+    #         except: #* If it's pre 1.7.2
+    #             data = {
+    #                 "name": str(level["Data"]["LevelName"]),
+    #                 "version": "Pre 1.7.2",
+    #                 "igt": utils.get_pre17_igt(mc_dir),
+    #                 "seen_credits": seen_credits,
+    #                 "pre17": True
+    #             }
+
+    #     return data
 
             
 
-    #-------------------------------------------매크로--------------------- 
+#-------------------------------------------macro--------------------- 
 
 
     def macro1(self):
@@ -94,7 +184,6 @@ class MainDialog(QMainWindow):
         pag.typewrite(igtPoint)
         pag.moveTo(1000, 500)
         pag.click()
-        print(Mods)
 
         if version == "1.16.1":
             pag.moveTo(1000,536)
@@ -142,7 +231,7 @@ class MainDialog(QMainWindow):
         if version == "1.16.1":
             pag.moveTo(1000,710)
             pag.click()
-        elif version == "1.14.4":
+        elif version == "1.14.4" or "1.15.2":
             pag.moveTo(1000,695)
             pag.click()
         else:
