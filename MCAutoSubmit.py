3e87e764fdeda58439d8b3aeddef20247b5a95d7
ProtossManse <ingan2473@gmail.com>
2021-07-21 19:46:39 +0900

    v1.1.0

--- MCAutoSubmit.py
+++ MCAutoSubmit.py
@@ -352,6 +352,7 @@ class MainDialog(QMainWindow, Ui_MainWindow):
     def closeEvent(self, QCloseEvent):
         WOWSANS.setValue("desc", self.descriptionText.toPlainText())
         WOWSANS.setValue("seedType", self.seedType.currentText())
+        WOWSANS.setValue("api", self.apiLine.text())
         
     
         
